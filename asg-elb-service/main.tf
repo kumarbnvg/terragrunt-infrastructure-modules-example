8f9d4f066af52e867ad1161e70e0a13c22cde604
kumarbnvg <92846577+kumarbnvg@users.noreply.github.com>
2023-06-15 17:48:24 +0530

    Update main.tf

--- asg-elb-service/main.tf
+++ asg-elb-service/main.tf
@@ -28,7 +28,7 @@ terraform {
 
 resource "aws_autoscaling_group" "webserver_example" {
   launch_configuration = aws_launch_configuration.webserver_example.id
-  vpc_zone_identifier  = data.aws_subnet.default.ids
+  vpc_zone_identifier  = data.aws_subnet.default.id
 
   load_balancers    = [aws_elb.webserver_example.name]
   health_check_type = "ELB"
